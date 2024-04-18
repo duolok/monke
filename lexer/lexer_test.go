0404e1e5740b64cb917f41595c126495374d0683
duolok <dusan.lecic5@example.com>
2024-04-18 20:21:55 +0200

    fix: input identation mistakes

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,16 +6,7 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-    input := `
-    let five = 5;
-    let ten = 10;
-
-    let add = fn(x, y) {
-        x + y;
-    };
-
-    let result = add(five, ten):
-    `;
+    input := `let five = 5; let ten = 10; let add = fn(x, y) { x + y; }; let result = add(five, ten);`
 
     tests := []struct{
         expctedType     token.TokenType
