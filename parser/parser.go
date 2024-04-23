3587f6a8a1e5e5a94fed893f1a6c1a12a2fd4d90
duolok <dusan.lecic5@gmail.com>
2024-04-23 07:08:41 +0200

    add: check errors

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+    "fmt"
     "monke/ast"
     "monke/lexer"
     "monke/token"
@@ -10,16 +11,24 @@ type Parser struct {
     l           *lexer.Lexer
     curToken    token.Token
     peekToken   token.Token
+    errors      []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-    p := &Parser{l: l}
+    p := &Parser{
+        l: l,
+        errors: []string{},
+    }
 
     // two tokens so curToken and peekToken are set
     p.nextToken()
     p.nextToken()
 
     return p
+}
+
+func (p *Parser) Errors() []string {
+    return p.errors
 }
 
 func (p *Parser) nextToken() {
@@ -83,6 +92,12 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
         p.nextToken()
         return true
     } else {
+        p.peekError(t)
         return false
     }
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+    msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+    p.errors = append(p.errors, msg)
 }
