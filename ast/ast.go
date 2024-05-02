78639c9b02b0eecda4311a865de438681e2e50be
duolok <dusan.lecic5@gmail.com>
2024-05-02 21:10:38 +0200

    add: expression statement and string method for AST nodes

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,15 @@
 package ast
 
+import (
+    "fmt"
+    "bytes"
+)
+
 import "monke/token"
 
 type Node interface {
     TokenLiteral() string
+    String() string
 }
 
 type Statement interface {
@@ -26,6 +32,16 @@ func (p *Program) TokenLiteral() string {
     } else {
         return ""
     }
+}
+
+func (p *Program) String() string {
+    var out bytes.Buffer
+    
+    for _, s := range p.Statements {
+        out.WriteString(s.String())
+    }
+
+    return out.String()
 }
 
 type LetStatement struct {
@@ -52,3 +68,48 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode() {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+type ExpressionStatement struct {
+    Token token.Token
+    Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (ls *LetStatement) String() string {
+    var out bytes.Buffer 
+
+    out.WriteString(ls.TokenLiteral() + " ")
+    out.WriteString(ls.Name.String())
+    out.WriteString(" = ")
+
+    if ls.Value != nil {
+        out.WriteString(ls.Value.String())
+    }
+
+    out.WriteString(";")
+    return out.String()
+}
+
+func (i *Identifier) String() string { return i.Value }
+
+func (rs *ReturnStatement) String() string {
+    var out bytes.Buffer
+
+    out.WriteString(rs.TokenLiteral() + " ")
+    if rs.ReturnValue != nil {
+        out.WriteString(rs.ReturnValue.String())
+    }
+
+    out.WriteString(";")
+    return out.String()
+}
+
+func (es *ExpressionStatement) String() string {
+    if es.Expression != nil {
+        return es.Expression.String()
+    }
+    return ""
+}
+
