6c49578a52fbef90687db618cec25a41ece7e3a1
duolok <dusan.lecic5@gmail.com>
2024-08-13 18:57:27 +0200

    add: pratt parsing basic prefix expressions

--- parser/parser.go
+++ parser/parser.go
@@ -7,6 +7,17 @@ import (
     "monke/token"
 )
 
+const (
+    _ int = iota
+    LOWEST
+    EQUALS
+    LESSGREATER
+    SUM
+    PRODUCT
+    PREFIX
+    CALL
+)
+
 type Parser struct {
     l           *lexer.Lexer
     curToken    token.Token
@@ -32,6 +43,9 @@ func New(l *lexer.Lexer) *Parser {
     // two tokens so curToken and peekToken are set
     p.nextToken()
     p.nextToken()
+
+    p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
+    p.registerPrefix(token.IDENT, p.parseIdentifier)
 
     return p
 }
@@ -60,6 +74,10 @@ func (p *Parser) ParseProgram() *ast.Program {
     return program
 }
 
+func (p *Parser) parseIdentifier() ast.Expression {
+    return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
 func (p *Parser) parseStatement() ast.Statement {
     switch p.curToken.Type {
         case token.LET:
@@ -67,7 +85,7 @@ func (p *Parser) parseStatement() ast.Statement {
         case token.RETURN:
             return p.parseReturnStatement()
         default: 
-            return nil
+            return p.parseExpressionStatement()
     }
 }
 
@@ -92,12 +110,33 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 
 func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
     stmt := &ast.ReturnStatement{Token: p.curToken}
+
     p.nextToken()
 
     for !p.curTokenIs(token.SEMICOLON) {
         p.nextToken()
     }
     return stmt
+}
+
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+    stmt := &ast.ExpressionStatement{Token: p.curToken}
+
+    stmt.Expression = p.parseExpression(LOWEST)
+
+    if p.peekTokenIs(token.SEMICOLON) {
+        p.nextToken()
+    }
+    return stmt
+}
+
+func (p *Parser) parseExpression(precedence int) ast.Expression {
+    prefix := p.prefixParseFns[p.curToken.Type]
+    if prefix == nil {
+        return nil 
+    }
+    leftExp := prefix()
+    return leftExp
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
