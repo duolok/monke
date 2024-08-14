9003b7f78ddc37ccd65b8caff20c7e8537ded40b
duolok <dusan.lecic5@gmail.com>
2024-08-14 12:59:57 +0200

    add: block statments for ifs

--- ast/ast.go
+++ ast/ast.go
@@ -43,15 +43,20 @@ type InfixExpression struct {
 }
 
 type Boolean struct {
-	Token	token.Token
-	Value	bool
+	Token token.Token
+	Value bool
 }
 
 type IfExpression struct {
-	Token		token.Token
-	Condition	Expression 
+	Token       token.Token
+	Condition   Expression
 	Consequence *BlockStatement
 	Alternative *BlockStatement
+}
+
+type BlockStatement struct {
+	Token      token.Token
+	Statements []Statement
 }
 
 func (p *Program) TokenLiteral() string {
@@ -172,19 +177,19 @@ func (oe *InfixExpression) String() string {
 	return out.String()
 }
 
-func (b *Boolean) expressionNode()		{}
+func (b *Boolean) expressionNode()      {}
 func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
-func (b *Boolean) String()	string		{ return b.Token.Literal }
+func (b *Boolean) String() string       { return b.Token.Literal }
 
-func (ie *IfExpression) expressionNode()		{}
-func (ie *IfExpression) TokenLiteral() string	{ return ie.Token.Literal }
-func (ie *IfExpression) String()	string {
+func (ie *IfExpression) expressionNode()      {}
+func (ie *IfExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *IfExpression) String() string {
 	var out bytes.Buffer
 
 	out.WriteString("if")
 	out.WriteString(ie.Condition.String())
 	out.WriteString(" ")
-	out.Write(ie.Consequence.String())
+	out.WriteString(ie.Consequence.String())
 
 	if ie.Alternative != nil {
 		out.WriteString("else ")
@@ -193,3 +198,16 @@ func (ie *IfExpression) String()	string {
 
 	return out.String()
 }
+
+func (bs *BlockStatement) statementNode()       {}
+func (bs *BlockStatement) TokenLiteral() string { return bs.Token.Literal }
+func (bs *BlockStatement) String() string {
+	var out bytes.Buffer
+
+	for _, s := range bs.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
