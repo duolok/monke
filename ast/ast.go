b03793949c6f196dfeb5d91d2bc8a9414942a9bb
duolok <dusan.lecic5@gmail.com>
2024-08-14 11:51:43 +0200

    add: boolean struct type

--- ast/ast.go
+++ ast/ast.go
@@ -42,6 +42,11 @@ type InfixExpression struct {
 	Right    Expression
 }
 
+type Boolean struct {
+	Token	token.Token
+	Value	bool
+}
+
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
@@ -159,3 +164,7 @@ func (oe *InfixExpression) String() string {
 
 	return out.String()
 }
+
+func (b *Boolean) expressionNode()		{}
+func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
+func (b *Boolean) String()	string		{ return b.Token.Literal }
