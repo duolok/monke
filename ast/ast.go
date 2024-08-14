3d42c717cf5a2bd5870f5b12eacf0ae46708d341
duolok <dusan.lecic5@gmail.com>
2024-08-14 15:32:39 +0200

    add: call expression type

--- ast/ast.go
+++ ast/ast.go
@@ -66,6 +66,12 @@ type FunctionLiteral struct {
 	Body		*BlockStatement
 }
 
+type CallExpression struct {
+	Token		token.Token
+	Function	Expression // '(' token
+	Arguments	[]Expression // identifier of functionLiteral
+}
+
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
@@ -238,41 +244,20 @@ func (fl *FunctionLiteral) String() string {
 	return out.String()
 }
 
+func (ce *CallExpression) expressionNode()				{}
+func (ce *CallExpression) TokenLiteral()	string { return ce.Token.Literal }
+func (ce *CallExpression) String() string {
+	var out	bytes.Buffer
+	
+	args := []string{}
+	for _, a := range ce.Arguments {
+		args = append(args, a.String())
+	}
 
+	out.WriteString(ce.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ", "))
+	out.WriteString(")")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	return out.String()
+}
