92855d79df89880ab57fa4e809ea1f05824a6752
duolok <dusan.lecic5@gmail.com>
2024-08-15 08:38:54 +0200

    add: null representation

--- object/object.go
+++ object/object.go
@@ -9,6 +9,7 @@ type ObjectType string
 const (
     INTEGER_OBJ = "INTEGER"
     BOOLEAN_OBJ = "BOOLEAN"
+    NULL_OBJ    = "NULL"
 )
 
 type Objecet interface {
@@ -17,16 +18,15 @@ type Objecet interface {
 }
 
 
-type Integer struct {
-    Value   int64
-}
-
-type Boolean struct {
-    Value   bool
-}
+type Integer struct { Value   int64 }
+type Boolean struct { Value   bool }
+type Null    struct {}
 
 func (i *Integer) Inspect() string      { return fmt.Sprintf("%d", i.Value) }
 func (i *Integer) Type()    ObjectType  { return INTEGER_OBJ }
 
 func (b *Boolean) Inspect() string      { return fmt.Sprintf("%t", b.Value) }
 func (b *Boolean) Type()    ObjectType  { return BOOLEAN_OBJ }
+
+func (n *Null)    Inspect() string      { return "null" }
+func (n *Null)    Type()    ObjectType  { return NULL_OBJ }
