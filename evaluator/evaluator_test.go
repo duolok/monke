293265c5a813a11d22a1aaa80271faad98359703
duolok <dusan.lecic5@gmail.com>
2024-08-15 10:28:49 +0200

    add: more boolean expression tests

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -66,12 +66,19 @@ func TestEvalBooleanExpression(t *testing.T) {
 	}{
 		{"true", true},
 		{"false", false},
+		{"1 < 2", true},
+		{"1 > 2", false},
+		{"1 < 1", false},
+		{"1 > 1", false},
+		{"1 == 1", true},
+		{"1 != 1", false},
+		{"1 == 2", false},
+		{"1 != 2", true},
 	}
 
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
 		testBooleanObject(t, evaluated, tt.expected)
-
 	}
 }
 
@@ -81,13 +88,11 @@ func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
 		t.Errorf("object is not Boolean. got=%T (%+v)", obj, obj)
 		return false
 	}
-
 	if result.Value != expected {
 		t.Errorf("object has wrong value. got=%t, want=%t",
 			result.Value, expected)
 		return false
 	}
-
 	return true
 }
 
